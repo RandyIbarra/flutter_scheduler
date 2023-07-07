ac4e29db8c2311d0795833329d6e21d4eb3e67b4
Randy Ibarra <randyosbaldo@gmail.com>
2023-07-07 17:19:24 -0600

    chore: refactor

--- lib/app.dart
+++ lib/app.dart
@@ -3,24 +3,62 @@ import 'package:flutter/material.dart';
 import 'package:flutter_scheduler/screens/mainscreen.dart';
 
 class SchedulerApp extends StatefulWidget {
-  const SchedulerApp({
-    Key? key,
-    required this.college,
-  }) : super(key: key);
-
-  final Map college;
+  const SchedulerApp({Key? key}) : super(key: key);
 
   @override
   State<SchedulerApp> createState() => _SchedulerAppState();
 }
 
 class _SchedulerAppState extends State<SchedulerApp> {
-  late Map college;
+  late Map<String, dynamic> college;
 
   @override
   void initState() {
     super.initState();
-    college = widget.college;
+    List<String> activityClassrooms = [
+      'Seminarios',
+      'Astrofisica',
+      'DEMAT-3',
+      'DEMAT-4',
+      'DEMAT-5',
+      'DEMAT-6',
+      'DEMAT-7',
+      'DEMAT-8'
+    ];
+    List<String> activityDays = [
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday'
+    ];
+    List<String> activitySchedules = [
+      '08:00-09:20',
+      '09:30-10:50',
+      '11:00-12:20',
+      '12:30-13:50',
+      '15:00-16:20'
+    ];
+
+    Map info = {};
+    for (String classroomName in activityClassrooms) {
+      Map classroom = {};
+      for (String activityDay in activityDays) {
+        Map days = {};
+        for (String activitySchedule in activitySchedules) {
+          days[activitySchedule] = '';
+        }
+        classroom[activityDay] = days;
+      }
+      info[classroomName] = classroom;
+    }
+
+    college = {
+      "activity_days": activityDays,
+      "activity_schedules": activitySchedules,
+      "activity_classrooms": activityClassrooms,
+      "info": info
+    };
   }
 
   @override
--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,9 @@
+// flutter
 import 'package:flutter/material.dart';
+// this project
 import 'package:flutter_scheduler/app.dart';
 
+/// Main function: Runs Scheduler App
 void main() {
-  List<String> activityClassrooms = [
-    'Seminarios',
-    'Astrofisica',
-    'DEMAT-3',
-    'DEMAT-4',
-    'DEMAT-5',
-    'DEMAT-6',
-    'DEMAT-7',
-    'DEMAT-8'
-  ];
-  List<String> activityDays = [
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday'
-  ];
-  List<String> activitySchedules = [
-    '08:00-09:20',
-    '09:30-10:50',
-    '11:00-12:20',
-    '12:30-13:50',
-    '15:00-16:20'
-  ];
-
-  Map info = {};
-  for (String classroomName in activityClassrooms) {
-    Map classroom = {};
-    for (String activityDay in activityDays) {
-      Map days = {};
-      for (String activitySchedule in activitySchedules) {
-        days[activitySchedule] = '';
-      }
-      classroom[activityDay] = days;
-    }
-    info[classroomName] = classroom;
-  }
-
-  Map college = {
-    "activity_days": activityDays,
-    "activity_schedules": activitySchedules,
-    "activity_classrooms": activityClassrooms,
-    "info": info
-  };
-
-  runApp(
-    SchedulerApp(
-      college: college,
-    ),
-  );
+  runApp(const SchedulerApp());
 }
