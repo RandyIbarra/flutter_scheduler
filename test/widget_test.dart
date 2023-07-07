c42bcab5fc140498882815b8d3ff9ae4b8db8cfb
Randy Ibarra <randyosbaldo@gmail.com>
2023-07-07 16:00:26 -0600

    fix: test errors

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,58 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_scheduler/main.dart';
+import 'package:flutter_scheduler/app.dart';
 
 void main() {
+  List<String> activity_classrooms = [
+    'Seminarios',
+    'Astrofisica',
+    'DEMAT-3',
+    'DEMAT-4',
+    'DEMAT-5',
+    'DEMAT-6',
+    'DEMAT-7',
+    'DEMAT-8'
+  ];
+  List<String> activity_days = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday'
+  ];
+  List<String> activity_schedules = [
+    '08:00-09:20',
+    '09:30-10:50',
+    '11:00-12:20',
+    '12:30-13:50',
+    '15:00-16:20'
+  ];
+
+  Map info = {};
+  for (String classroom_name in activity_classrooms) {
+    Map classroom = {};
+    for (String activity_day in activity_days) {
+      Map days = {};
+      for (String activity_schedule in activity_schedules) {
+        days[activity_schedule] = '';
+      }
+      classroom[activity_day] = days;
+    }
+    info[classroom_name] = classroom;
+  }
+
+  Map college = {
+    "activity_days": activity_days,
+    "activity_schedules": activity_schedules,
+    "activity_classrooms": activity_classrooms,
+    "info": info
+  };
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(
+      MyApp(college: college),
+    );
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
