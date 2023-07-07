50ad60da66f369232df30d34cc43f30f5392f972
Randy Ibarra <randyosbaldo@gmail.com>
2023-07-07 16:02:53 -0600

    chore: code format

--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:flutter_scheduler/app.dart';
 
 void main() {
-  List<String> activity_classrooms = ['Seminarios', 'Astrofisica', 'DEMAT-3', 'DEMAT-4', 'DEMAT-5', 'DEMAT-6', 'DEMAT-7', 'DEMAT-8'];
-  List<String> activity_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
-  List<String> activity_schedules = ['08:00-09:20', '09:30-10:50', '11:00-12:20', '12:30-13:50', '15:00-16:20'];
-  
+  List<String> activityClassrooms = [
+    'Seminarios',
+    'Astrofisica',
+    'DEMAT-3',
+    'DEMAT-4',
+    'DEMAT-5',
+    'DEMAT-6',
+    'DEMAT-7',
+    'DEMAT-8'
+  ];
+  List<String> activityDays = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday'
+  ];
+  List<String> activitySchedules = [
+    '08:00-09:20',
+    '09:30-10:50',
+    '11:00-12:20',
+    '12:30-13:50',
+    '15:00-16:20'
+  ];
+
   Map info = {};
-  for(String classroom_name in activity_classrooms){
+  for (String classroomName in activityClassrooms) {
     Map classroom = {};
-    for(String activity_day in activity_days){
+    for (String activityDay in activityDays) {
       Map days = {};
-      for(String activity_schedule in activity_schedules){
-        days[activity_schedule] = '';
+      for (String activitySchedule in activitySchedules) {
+        days[activitySchedule] = '';
       }
-      classroom[activity_day] = days;
+      classroom[activityDay] = days;
     }
-    info[classroom_name] = classroom;
+    info[classroomName] = classroom;
   }
 
   Map college = {
-    "activity_days": activity_days,
-    "activity_schedules": activity_schedules,
-    "activity_classrooms": activity_classrooms, 
+    "activity_days": activityDays,
+    "activity_schedules": activitySchedules,
+    "activity_classrooms": activityClassrooms,
     "info": info
   };
 
