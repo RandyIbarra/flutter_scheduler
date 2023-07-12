f7752e9c6940ada0fa718724c1288d565abebc86
Randy Ibarra <randyosbaldo@gmail.com>
2023-07-12 00:59:45 -0600

    feat: firebase added to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 // flutter
 import 'package:flutter/material.dart';
+// external packages
+import 'package:firebase_core/firebase_core.dart';
 // this project
+import 'firebase_options.dart';
 import 'package:flutter_scheduler/app.dart';
 
 /// Main function: Runs Scheduler App
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const SchedulerApp());
 }
